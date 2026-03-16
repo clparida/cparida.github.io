2ea93f4ea85084fb1b05e7b75356691e319e8979
Chandanlal Parida <102585840+clparida@users.noreply.github.com>
2026-03-16 10:01:19 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,26 @@
 <?php
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
-  $message = htmlspecialchars($_POST['message']);
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$affiliation = htmlspecialchars($_POST['affiliation']);
+$position = htmlspecialchars($_POST['position']);
+$message = htmlspecialchars($_POST['message']);
 
-  if(!empty($email) && !empty($message)){
+if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "chandanlal.parida@gmail.com"; //enter that email address where you want to receive all messages
-      $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$message\n\nRegards,\n$name";
-      $sender = "From: $email";
-      if(mail($receiver, $subject, $body, $sender)){
-         echo "Your message has been sent";
-      }else{
-         echo "Sorry, failed to send your message!";
-      }
+        $receiver = "chandanlal.parida@gmail.com"; // your email
+        $subject = "From: $name <$email>";
+        $body = "Name: $name\nEmail: $email\nAffiliation: $affiliation\nPosition: $position\nMessage:\n$message\n\nRegards,\n$name";
+        $sender = "From: $email";
+
+        if(mail($receiver, $subject, $body, $sender)){
+            echo "Your message has been sent";
+        }else{
+            echo "Sorry, failed to send your message!";
+        }
     }else{
-      echo "Enter a valid email address!";
+        echo "Enter a valid email address!";
     }
-  }else{
-    echo "Email and message field is required!";
-  }
+}else{
+    echo "Email and message field are required!";
+}
 ?>
